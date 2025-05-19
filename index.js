5de3b31ad09e11bc896a88c9695d4ce71aa4d092
Manasa <manasavinodkumar@gmail.com>
2025-05-19 03:15:45 -0400

    msg

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const concertRoutes = require('./routes/concerts');
 const authRoutes = require('./routes/auth');
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
